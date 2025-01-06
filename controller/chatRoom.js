579dc94da357c6d63d7a29fc65e634b2d84e814a
shazaniyu55 <shazaniyu@gmail.com>
2025-01-06 03:41:14 +0100

    new port

--- controller/chatRoom.js
+++ controller/chatRoom.js
@@ -2,7 +2,7 @@
 import makeValidation from '@withvoid/make-validation';
 // models
 import ChatRoomModel, { CHAT_ROOM_TYPES } from '../model/chatRoom.js';
-import ChatMessageModel from '../model/ChatMessage.js';
+import ChatMessageModel from '../model/chatMessage.js';
 import UserModel from '../model/user.js';
 
 export default {
